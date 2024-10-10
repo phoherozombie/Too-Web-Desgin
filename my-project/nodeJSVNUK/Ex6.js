dbf30b81ad27cc358d8bfcea4a9d1f1f76032df9
phoherozombie <phucdo3687076@gmail.com>
2024-10-10 18:15:59 +0700

    commit

---
+++ my-project/nodeJSVNUK/Ex6.js
@@ -0,0 +1,11 @@
+//Function caculate Permimeter 
+const caculatePermimeter= (width, length) => (width +length)*2;
+// Function caculate Area
+const caculateArea = (width, length) => width*length;
+
+let width=8;
+let length=12;
+
+console.log("Permimeter: "+ caculatePermimeter(width,length));
+console.log("Area: "+caculateArea(width,length));
+
---
+++ my-project/nodeJSVNUK/varlet.js
@@ -0,0 +1,5 @@
+var hello = (name,meesage) => {
+    console.log("Chao " + name + ', ban that ' + meesage);
+
+}
+hello('Tuyen',"xau trai hihi")
