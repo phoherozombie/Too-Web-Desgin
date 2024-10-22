251e5824140731a2bdc2d3b2174e923f540ee432
phoherozombie <151513205+phoherozombie@users.noreply.github.com>
2024-10-22 18:21:51 +0700

    Update varlet.js

--- my-project/nodeJSVNUK/varlet.js
+++ my-project/nodeJSVNUK/varlet.js
@@ -1,14 +1,24 @@
-class MyClass {
-    a = 1; 
-    #b = 2;
-    static #c = 3;
-  
-    incB(){
-      this.#b++;
-      console.log(this.#b)
-  
-    }
-  
-  }
-  const m = new MyClass();
-  m.incB()
+let myMap = new Map();
+
+myMap.set('name', 'John');
+myMap.set('age', 30);
+
+console.log(myMap.get('name')); // John
+console.log(myMap.size);        // 2
+
+myMap.delete('age');
+console.log(myMap.has('age'));  // false
+
+///////////////////////////////////
+
+let mySet = new Set();
+
+mySet.add(1);
+mySet.add(2);
+mySet.add(1); // Duplicate, will not be added
+
+console.log(mySet.has(1)); // true
+console.log(mySet.size);   // 2
+
+mySet.delete(1);
+console.log(mySet.has(1)); // false
