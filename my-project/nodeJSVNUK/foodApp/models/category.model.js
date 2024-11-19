de4eb660c0046c027aed293ee4bc4c3994f8b736
phoherozombie <phucdo3687076@gmail.com>
2024-11-19 13:27:19 +0700

    Update

---
+++ my-project/nodeJSVNUK/foodApp/models/category.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const categorySchema = mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    img: {type: String, required: true}
+})
+
+module.exports = mongoose.model('category', categorySchema)
